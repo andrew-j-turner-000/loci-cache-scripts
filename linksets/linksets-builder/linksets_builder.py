2a781a7a0562d946086cd0fda47f20a95a85a284
Benjamin Leighton <Ben.Leighton@csiro.au>
2019-08-08 06:58:04 +0000

    bad meshblocks identification and classifiers of within vs overlaps for near things

--- linksets/linksets-builder/linksets_builder.py
+++ linksets/linksets-builder/linksets_builder.py
@@ -10,11 +10,17 @@ from botocore.client import Config
 logging.basicConfig(level=logging.DEBUG)
 
 def run_command(command_line_array):
+    '''
+    Utility for running commands and logging outputs
+    '''
     output = ''
     output = subprocess.check_output(command_line_array, universal_newlines=True)
     logging.info(output)
 
 def prepare_database():
+    '''
+    Create and prepare a postgis database for geospatial processing
+    '''
     run_command(["psql", "--host", "postgis", "--user", "postgres", "-c", "DROP DATABASE IF EXISTS mydb;"])
     run_command(["psql", "--host", "postgis", "--user", "postgres", "-c", "CREATE DATABASE mydb;"])
     run_command(["psql", "--host", "postgis", "--user", "postgres", "-d", "mydb", 
@@ -42,6 +48,7 @@ def get_geofabric_assets():
 
 def load_geofabric_catchments():
     '''
+    Loads Geofabric Catchments into PostGIS
     Note: No need to set -nlt MULTIPOLYGON here, because the .gdb file already defines the shapes as MULTIPOLYGON.
     All Geofabric cooards are in crs EPSG:4326 (WGS-84), but the geometry column in the catchments table DOES NOT contain a SRID declaration, so you need to add it yourself in the query
     eg: ST_GeomFromWKB(wkb_geometry, 4326). Then we need to convert it to albers (EPSG:3577) in order to do constant-area intersections with meshblocks.
@@ -49,7 +56,123 @@ def load_geofabric_catchments():
     run_command(["ogr2ogr", "-f", "PostgreSQL", "PG:host=postgis port=5432 dbname=mydb user=postgres password=password", 
         "../assets/HR_Catchments_GDB/HR_Catchments.gdb", "-skipfailures", "-overwrite", "-progress", "--config", "PG_USE_COPY", "YES"])
 
+
+def harmonize_crs_albers():
+    '''
+    Harmonize CRS to albers (EPSG:3577) in otder to do constant-area intersections with meshblocks
+    '''
+    harmonize_crs_sql = """
+    ALTER TABLE public.\"mb:mb\" ADD COLUMN geom_3577 geometry(Geometry,3577);
+    UPDATE public.\"mb:mb\" SET geom_3577 = ST_MakeValid(ST_Transform(wkb_geometry,3577));
+    ALTER TABLE public.\"ahgfcontractedcatchment\" ADD COLUMN geom_3577 geometry(Geometry,3577);
+    UPDATE public.\"ahgfcontractedcatchment\" SET geom_3577 = ST_Transform(ST_MakeValid(ST_GeomFromWKB(wkb_geometry, 4326)),3577);
+    """
+    run_command(["psql", "--host", "postgis", "--user", "postgres", "-c", harmonize_crs_sql])
+
+
+def create_geometry_indexes():
+    '''
+    Create indexes on geometry columns for performance
+    '''
+    create_geometry_indexes_sql = """
+    CREATE INDEX mb_geom_3577_gix ON public.\"mb:mb\" USING GIST (geom_3577);
+    CREATE INDEX cc_geom_3577_gix ON public.\"ahgfcontractedcatchment\" USING GIST (geom_3577);
+    """
+    run_command(["psql", "--host", "postgis", "--user",
+                 "postgres", "-c", create_geometry_indexes_sql])
+
+def create_intersections():
+    create_intersection_sql = """
+CREATE MATERIALIZED VIEW mbintersectcc_mv AS
+SELECT mb.mb_code_2016, ca.hydroid, ST_Intersection(mb.geom_3577, ca.geom_3577) as i
+FROM public.\"ahgfcontractedcatchment\" as ca
+INNER JOIN public.\"mb:mb\" as mb ON mb.geom_3577 && ca.geom_3577 -- the && specifies an indexed bounding box lookup
+WHERE ST_IsValid(ca.geom_3577) AND ST_IsValid(mb.geom_3577) AND ST_Intersects(mb.geom_3577, ca.geom_3577)
+ORDER BY mb.mb_code_2016 ASC;
+CREATE MATERIALIZED VIEW ccintersectmb_mv AS
+SELECT ca.hydroid, mb.mb_code_2016, ST_Intersection(ca.geom_3577, mb.geom_3577) as i
+FROM public.\"mb:mb\" as mb
+INNER JOIN public.\"ahgfcontractedcatchment\" as ca ON ca.geom_3577 && mb.geom_3577 -- the && specifies an indexed bounding box lookup
+WHERE ST_IsValid(ca.geom_3577) AND ST_IsValid(mb.geom_3577) AND ST_Intersects(ca.geom_3577, mb.geom_3577)
+ORDER BY ca.hydroid ASC;
+"""
+    run_command(["psql", "--host", "postgis", "--user",
+                 "postgres", "-c", create_geometry_indexes_sql])
+
+
+def create_intersections_areas():
+    create_intersection_areas_sql = """
+CREATE VIEW mbintersectccareas AS
+SELECT s.mb_code_2016, s.hydroid, s.mb_area, s.cc_area, s.i_area, (s.i_area / s.mb_area) as mb_proportion, (s.i_area / s.cc_area) as cc_proportion, s.geomcollection FROM (
+    SELECT mv.mb_code_2016,
+           mv.hydroid,
+           ST_Area(mb.geom_3577)                         as mb_area,
+           ST_Area(ca.geom_3577)                         as cc_area,
+           ST_Area(mv.i)                                 as i_area,
+           ST_Collect(ARRAY[ca.geom_3577, mb.geom_3577, mv.i]) as geomcollection
+    FROM mbintersectcc_mv as mv
+    INNER JOIN public.\"mb:mb\" as mb ON mb.mb_code_2016 = mv.mb_code_2016
+    INNER JOIN public.\"ahgfcontractedcatchment\" as ca ON ca.hydroid = mv.hydroid
+) as s;
+CREATE VIEW ccintersectmbareas AS
+SELECT s.hydroid, s.mb_code_2016, s.cc_area, s.mb_area, s.i_area, (s.i_area / s.cc_area) as cc_proportion, (s.i_area / s.mb_area) as mb_proportion, s.geomcollection FROM (
+    SELECT mv.hydroid,
+           mv.mb_code_2016,
+           ST_Area(ca.geom_3577)                         as cc_area,
+           ST_Area(mb.geom_3577)                         as mb_area,
+           ST_Area(mv.i)                                 as i_area,
+           ST_Collect(ARRAY[mb.geom_3577, ca.geom_3577, mv.i]) as geomcollection
+    FROM ccintersectmb_mv as mv
+    INNER JOIN public.\"ahgfcontractedcatchment\" as ca ON ca.hydroid = mv.hydroid
+    INNER JOIN public.\"mb:mb\" as mb ON mb.mb_code_2016 = mv.mb_code_2016
+) as s;
+"""
+    run_command(["psql", "--host", "postgis", "--user",
+                 "postgres", "-c", create_intersection_areas_sql])
+
+def find_bad_meshblocks():
+    '''
+    Find bad mesblocks
+    Meshblocks smaller than an specified area and meshblock proportion (is that amount in the meshblock when it intersects?)
+    is larger than a small ratio
+    or 
+    Mesblocks where intersecting area smaller than a specified area 
+    and catchment proportion (maybe the amount intersecting that is in the catchment) is larger than a small ratio 
+    '''
+    find_bad_meshblocks_sql =
+    """
+    CREATE MATERIALIZED VIEW bad_meshblocks as
+    SELECT mb.mb_code_2016, mb.hydroid FROM mbintersectccareas as mb
+    WHERE mb.mb_area < 1100.0 and ((mb_proportion >= 0.010)or(cc_proportion >= 0.010)) and i_area <= 50.0;
+    """
+    run_command(["psql", "--host", "postgis", "--user",
+                 "postgres", "-c", find_bad_meshblocks_sql])
+
+def create_classifier_views():
+    '''
+    annotate meshblocks and cc to indicate whether they are considered truely overlapping or close enough to be within
+    when they intersect 
+    '''
+    create_classifier_views_sql =
+    """
+    CREATE VIEW mbintersectccareas_classify AS
+    SELECT mb.*, mb_proportion >= 0.010 as is_overlaps, mb_proportion >=0.990 as is_within
+    FROM mbintersectccareas as mb;
+    -- (TODO: minus bad meshblocks)
+    CREATE VIEW ccintersectmbareas_classify AS
+    SELECT cc.*, cc_proportion >= 0.010 as is_overlaps, cc_proportion >=0.990 as is_within
+    FROM ccintersectmbareas as cc;
+    """
+    run_command(["psql", "--host", "postgis", "--user",
+                 "postgres", "-c", create_classifier_views_sql])
+
 prepare_database()
 load_asgs_mb()
 get_geofabric_assets()
 load_geofabric_catchments()
+harmonize_crs_albers()
+create_geometry_indexes()
+create_intersections()
+create_intersections_areas()
+find_bad_meshblocks()
+create_classifier_views()
