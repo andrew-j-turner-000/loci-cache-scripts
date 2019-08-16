2abc6dce7a0a4eaad9ba57412c9f7c7da1fc6f96
Seaton, Shane (L&W, Black Mountain) <shane.seaton@csiro.au>
2019-08-16 12:49:45 +1000

    Update asgs 2016 harvester to use common vars

--- docker/harvest/asgs/main.sh
+++ docker/harvest/asgs/main.sh
@@ -1,16 +1,13 @@
 #!/bin/sh
 set -e
 
-DATASET_NAME="asgs2016"
-GRAPH_NAME="http://linked.data.gov.au/dataset/asgs2016"
-OUTPUT_FILE="${DATASET_NAME}.trig.gz"
-
-S3_BUCKET="loci-assets"
-S3_REGION="ap-southeast-2"
-S3_PATH="auto-generated/datasets"
-
 if [ -z "$AWS_ACCESS_KEY_ID" ]; then echo "AWS_ACCESS_KEY_ID User needs to be set."; exit 1; fi
 if [ -z "$AWS_SECRET_ACCESS_KEY" ]; then echo "AWS_SECRET_ACCESS_KEY User needs to be set."; exit 1; fi
+if [ -z "$OUTPUT_FILE" ]; then echo "OUTPUT_FILE needs to be set."; exit 1; fi
+if [ -z "$GRAPH_NAME" ]; then echo "GRAPH_NAME needs to be set."; exit 1; fi
+if [ -z "$S3_BUCKET" ]; then echo "S3_BUCKET needs to be set."; exit 1; fi
+if [ -z "$S3_REGION" ]; then echo "S3_REGION needs to be set."; exit 1; fi
+if [ -z "$S3_PATH" ]; then echo "S3_PATH needs to be set."; exit 1; fi
 
 git clone https://github.com/CSIRO-enviro-informatics/asgs-dataset.git
 cd asgs-dataset
