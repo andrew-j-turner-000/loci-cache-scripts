5cf5d23071acf4c1428ca203a523e0e0c0175f47
Ben Leighton <Ben.Leighton@csiro.au>
2019-08-27 10:10:07 +1000

    linksets_triples_builder executes all the way through against a linksets_builder created database and produce output ttl files

--- docker/linksets/linksets-builder/linksets_triples_builder.py
+++ docker/linksets/linksets-builder/linksets_triples_builder.py
@@ -1,70 +1,57 @@
 import psycopg2 as pg
-​
+import os
+database_url = os.getenv('DATABASE_URL')
 mb_area_template = """\
 mb:{mb_code_2016:s} a f: ;
  am2: [ dv: "{mb_area_m2:f}"^^dc: ; crs: albers: ] .
-​
 """
-​
 cc_area_template = """\
 cc:{hydroid:s} a f: ;
  am2: [ dv: "{cc_area_m2:f}"^^dc: ; crs: albers: ] .
-​
 """
-​
 overlaps_template = """\
 :i{intersection_iter:d} a f: ;
  am2: [ dv: "{i_area_m2:f}"^^dc: ; crs: albers: ] .
-​
 :mo{intersection_iter:d} s: mb:{mb_code_2016:s} ;
  p: c: ;
  o: :i{intersection_iter:d} ;
  i: l: ;
  m: _:mb16cc_p .
-​
 :co{intersection_iter:d} s: cc:{hydroid:s} ;
  p: c: ;
  o: :i{intersection_iter:d} ;
  i: l: ;
  m: _:mb16cc_p .
-​
 :to{intersection_iter:d} s: mb:{mb_code_2016:s} ;
  p: tso: ;
  o: cc:{hydroid:s};
  i: l: ;
  m: _:mb16cc_p .
-​
 """
-​
 mb_sf_within_template = """\
 :mw{within_iter:d} s: mb:{mb_code_2016:s} ;
  p: w: ;
  o: cc:{hydroid:s} ;
  i: l: ;
  m: _:mb16cc_p .
-​
 """
-​
 mb_sf_contains_template = """\
 :mc{within_iter:d} s: mb:{mb_code_2016:s} ;
  p: c: ;
  o: cc:{hydroid:s} ;
  i: l: ;
  m: _:mb16cc_p .
-​
 """
-​
-​
 def do_overlaps():
-    con = pg.connect("host=localhost port=5532 dbname=mydb user=postgres password=password")
+    con = pg.connect("host={} port=5432 dbname=mydb user=postgres password=password".format(database_url))
     cur = con.cursor("cur1")
     cur.execute("""\
     SELECT mb.mb_code_2016, cc.hydroid, mb.mb_area, cc.cc_area, mb.i_area, mb.is_overlaps, cc.is_overlaps, mb.is_within, cc.is_within 
-    FROM public."mbalbersintersectccareas_classify" as mb
-    INNER JOIN public."ccalbersintersectmbareas_classify" as cc on mb.mb_code_2016 = cc.mb_code_2016 and mb.hydroid = cc.hydroid
+    FROM public."mbintersectccareas_classify" as mb
+    INNER JOIN public."ccintersectmbareas_classify" as cc on mb.mb_code_2016 = cc.mb_code_2016 and mb.hydroid = cc.hydroid
     WHERE (mb.is_overlaps or cc.is_overlaps) and (not mb.is_within) and (not cc.is_within);
-    --LIMIT 10;
-    --ORDER BY mb.mb_code_2016;
+    -- LIMIT 10;
+    -- ORDER BY mb.mb_code_2016;
     """)
     c = 0
     intersection_iter = 0
@@ -82,7 +69,6 @@ def do_overlaps():
             mb_area_m2 = round((mb_area_m2 / 100.0), 7) * 100.0
             cc_area_m2 = round((cc_area_m2 / 100.0), 7) * 100.0
             i_area_m2  = round((i_area_m2  / 100.0), 7) * 100.0
-​
             if mb_code_2016 not in expressed_mb_areas:
                 mb_area_chunk = mb_area_template.format(mb_code_2016=mb_code_2016, mb_area_m2=mb_area_m2)
                 outfile.write(mb_area_chunk)
@@ -93,20 +79,17 @@ def do_overlaps():
                 expressed_cc_areas.add(hydroid)
             next_chunk = overlaps_template.format(mb_code_2016=mb_code_2016, hydroid=hydroid, intersection_iter=intersection_iter, i_area_m2=i_area_m2)
             outfile.write(next_chunk)
-​
-​
 def do_withins():
-    con = pg.connect("host=localhost dbname=intersect_me user=postgres password=password")
+    con = pg.connect("host={} dbname=mydb user=postgres password=password".format(database_url))
     cur = con.cursor("cur2")
     cur.execute("""\
     SELECT mb.mb_code_2016, cc.hydroid, mb.is_within, cc.is_within
     FROM public."mbintersectccareas_classify" as mb
     INNER JOIN public."ccintersectmbareas_classify" as cc on mb.mb_code_2016 = cc.mb_code_2016 and mb.hydroid = cc.hydroid
     WHERE mb.is_within or cc.is_within;
-    --LIMIT 10;
-    --ORDER BY mb.mb_code_2016;
+    -- LIMIT 10;
+    -- ORDER BY mb.mb_code_2016;
     """)
-​
     c = 0
     within_iter = 0
     with open("within_all.ttl", "w") as outfile:
@@ -121,10 +104,7 @@ def do_withins():
                 next_chunk = mb_sf_within_template.format(mb_code_2016=mb_code_2016, hydroid=hydroid, within_iter=within_iter)
             else:
                 next_chunk = mb_sf_contains_template.format(mb_code_2016=mb_code_2016, hydroid=hydroid, within_iter=within_iter)
-​
             outfile.write(next_chunk)
-​
-​
 if __name__ == "__main__":
     do_withins()
     do_overlaps()
