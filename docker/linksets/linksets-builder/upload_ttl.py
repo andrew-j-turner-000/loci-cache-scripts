56838939ac7d9a972c5969ef020651fd5da7dc29
Ben Leighton <Ben.Leighton@csiro.au>
2019-08-29 18:45:07 +1000

    use right env variable

--- docker/linksets/linksets-builder/upload_ttl.py
+++ docker/linksets/linksets-builder/upload_ttl.py
@@ -5,7 +5,7 @@ import os
 import utils
 s3_bucket = utils.fail_or_getenv('S3_BUCKET')
 s3_linkset_path = utils.fail_or_getenv('S3_LINKSET_PATH')
-s3_region_name = utils.fail_or_getenv(('S3_REGION_NAME')
+s3_region_name = utils.fail_or_getenv('S3_REGION')
 s3_client = boto3.client('s3', region_name=s3_region_name)
 filename = s3_linkset_path+ '/' + "ls_mb16cc.ttl"
 s3_client.upload_file('./ls_mb16cc.ttl', s3_bucket, filename[1:])
