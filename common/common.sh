04cbc01bd32a89ac56abc983cc2566baf858b213
Seaton, Shane (L&W, Black Mountain) <shane.seaton@csiro.au>
2019-08-29 11:28:37 +1000

    Remove quotes from the common.env

--- common/common.sh
+++ common/common.sh
@@ -5,6 +5,6 @@ export AWS_ACCESS_KEY_ID="${LOCI_S3_ACCESS_KEY_ID}"
 export AWS_SECRET_ACCESS_KEY="${LOCI_S3_SECRET_ACCESS_KEY}"
 
 SCRIPT_HOME=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-echo $SCRIPT_HOME
+
 #import env vars to shell
 export $(grep -v '^#' ${SCRIPT_HOME}/common.env | sed '/^$/d' | xargs -0)
