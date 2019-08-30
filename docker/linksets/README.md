e1a9e89ddde78938295f82140084792c772efac4
Ben Leighton <Ben.Leighton@csiro.au>
2019-08-30 10:12:07 +1000

    Documentation updated with requirements and start script, env variables check in upload, full workflow in docker-compose

--- docker/linksets/README.md
+++ docker/linksets/README.md
@@ -1,3 +1,13 @@
 # Automation of Linkset Creation
 
-Code to automate the process of linkset creation.
+Code to automate the process of linkset creation. Currently specific for meshblocks and contracted catchments.
+
+## Requirements
+
+Requires environment variables configured in `../../common/common.sh` and in `./.env`
+
+additionally valid `AWS_ACCESS_KEY_ID` and `AWS_SECRET_ACCESS_KEY` must be set prior to run
+
+## Running
+
+Run with `start.sh`
--- docker/linksets/linksets-builder/upload_ttl.py
+++ docker/linksets/linksets-builder/upload_ttl.py
@@ -3,6 +3,13 @@ from botocore import UNSIGNED
 from botocore.client import Config
 import os
 import utils
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+logging.info("Uploading linkset to s3")
+
+utils.fail_or_getenv('AWS_ACCESS_KEY_ID')
+utils.fail_or_getenv('AWS_SECRET_ACCESS_KEY')
 s3_bucket = utils.fail_or_getenv('S3_BUCKET')
 s3_linkset_path = utils.fail_or_getenv('S3_LINKSET_PATH')
 s3_region_name = utils.fail_or_getenv('S3_REGION')
