2bcd62655e58c19fd4571f01ce5dd87cfd56a531
Ben Leighton <Ben.Leighton@csiro.au>
2019-09-16 16:14:18 +1000

    Factored out mb and cc strings from most of the generic logic and header. Beginning to parameterize these.

--- docker/linksets/mb2cc/linksets_triples_builder.py
+++ docker/linksets/mb2cc/linksets_triples_builder.py
@@ -8,11 +8,7 @@ from io import StringIO
 
 logging.basicConfig(level=logging.DEBUG)
 
-database_url = utils.fail_or_getenv('DATABASE_URL')
-contact_name = utils.fail_or_getenv('CONTACT_NAME')
-contact_email = utils.fail_or_getenv('CONTACT_EMAIL')
-
-header_template = """\
+header_template = """ 
 @prefix prov: <http://www.w3.org/ns/prov#> .
 @prefix qudt: <http://qudt.org/schema/qudt#> .
 @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
@@ -31,14 +27,15 @@ header_template = """\
 @prefix : <http://linked.data.gov.au/dataset/mb16cc/statement/> .
 @prefix i: <http://purl.org/dc/terms/isPartOf> .
 @prefix l: <http://linked.data.gov.au/dataset/mb16cc> .
-@prefix mb: <http://linked.data.gov.au/dataset/asgs2016/meshblock/> .
-@prefix cc: <http://linked.data.gov.au/dataset/geofabric/contractedcatchment/> .
+@prefix from: <{from_prefix}> .
+@prefix to: <{to_prefix}> .
 @prefix s: <http://www.w3.org/1999/02/22-rdf-syntax-ns#subject> .
 @prefix p: <http://www.w3.org/1999/02/22-rdf-syntax-ns#predicate> .
 @prefix o: <http://www.w3.org/1999/02/22-rdf-syntax-ns#object> .
 @prefix m: <http://linked.data.gov.au/def/loci/hadGenerationMethod> .
 @prefix w: <http://www.opengis.net/ont/geosparql#sfWithin> .
 @prefix c: <http://www.opengis.net/ont/geosparql#sfContains> .
+@prefix ov: <http://www.opengis.net/ont/geosparql#sfOverlaps> .
 @prefix tso: <http://linked.data.gov.au/def/geox#transitiveSfOverlap> .
 @prefix f: <http://www.opengis.net/ont/geosparql#Feature> .
 @prefix dc: <http://www.w3.org/2001/XMLSchema#decimal> .
@@ -48,25 +45,22 @@ header_template = """\
 @prefix albers: <http://www.opengis.net/def/crs/EPSG/0/3577> .
 
 l: a loci:Linkset ;
-  dct:title "Meshblocks Contracted Catchments Linkset" ;
-  dct:description \"\"\"This LOC-I Project Linkset relates Meshblock individuals in the ASGS LOC-I Dataset to Contracted Catchment individuals in the Geofabric LOC-I Dataset. All Meshblock -> Catchment relations are either transitiveSfOverlap, sfWithin, or sfContains. That is a Meshblock either overlaps or is wholly within the Catchment, or in some cases the Meshblock wholly contains the Catchment.
-The Linkset triples (Meshblock sfWithin Catchment, Meshblock sfContains Catchment, Meshblock transitiveSfOverlap Catchment) are reified so that each triple is contained within an RDF Statement class instance so that the triple is numbered and the method used to generate the triple is given by the loci:hadGenerationMethod.
-The method used for all triples in this Linkset is the same and it is SpatialIntersection which is defined below.
-The triples for the main data in this linkset - the Statements relating Meshblocks to Catchments - are valid RDF in the Turtle syntax but an unusual namespacing arrangement is used so all terms are indicated with as few letters as possible, mostly one letter then colon, e.g. s: for http://www.w3.org/1999/02/22-rdf-syntax-ns#subject, rather than the more common rdf:subject. This is simply to reduce file size.\"\"\"@en ;
+  dct:title {header_title} ;
+  dct:description \"\"\"{header_description}\"\"\"@en;
   dct:publisher <http://catalogue.linked.data.gov.au/org/O-000886> ;
   dc:publisher "CSIRO" ;
   dcat:contactPoint _:contact ;
   dct:issued "{creation_date}"^^xsd:date ;
   dct:modified "{creation_date}"^^xsd:date ;
-  void:subjectsTarget <http://linked.data.gov.au/dataset/asgs2016> ;
+  void:subjectsTarget <{subjects_target}> ;
   void:linkPredicate w: , c: , tso: ;
-  void:objectsTarget <http://linked.data.gov.au/dataset/geofabric> ;
-  m: _:mb16cc_p .
+  void:objectsTarget <{subjects_target}> ;
+  m: _:linkset_prov_p .
 
-_:mb16cc_p a prov:Plan ;
+_:linkset_prov_p a prov:Plan ;
     rdfs:label "Spatial Intersection Method" ;
     prov:value <https://github.com/CSIRO-enviro-informatics/loci-cache-scripts/tree/linksets-builder> ;
-    rdfs:comment "The method used to classify Meshblock and Contracted Catchment relationships" ;
+    rdfs:comment {provenance_comment};
     prov:wasAttributedTo _:contact;
     prov:generatedAtTime "{creation_date}"^^xsd:date .
 
@@ -75,48 +69,48 @@ _:contact a vcard:Individual ;
   vcard:hasEmail <{contact_email}> .
 """
 
-
-mb_area_template = """\
-mb:{from_id_column:s} a f: ;
- am2: [ dv: "{mb_area_m2:f}"^^dc: ; crs: albers: ] .
+from_area_template = """\
+from:{from_id_column:s} a f: ;
+ am2: [ dv: "{from_area_m2:f}"^^dc: ; crs: albers: ] .
 """
-cc_area_template = """\
-cc:{to_id_column:s} a f: ;
- am2: [ dv: "{cc_area_m2:f}"^^dc: ; crs: albers: ] .
+to_area_template = """\
+to:{to_id_column:s} a f: ;
+ am2: [ dv: "{to_area_m2:f}"^^dc: ; crs: albers: ] .
 """
 overlaps_template = """\
 :i{intersection_iter:d} a f: ;
  am2: [ dv: "{i_area_m2:f}"^^dc: ; crs: albers: ] .
-:mo{intersection_iter:d} s: mb:{from_id_column:s} ;
+:mo{intersection_iter:d} s: from:{from_id_column:s} ;
  p: c: ;
  o: :i{intersection_iter:d} ;
  i: l: ;
- m: _:mb16cc_p .
-:co{intersection_iter:d} s: cc:{to_id_column:s} ;
+ m: _:linkset_prov_p.
+:co{intersection_iter:d} s: to:{to_id_column:s} ;
  p: c: ;
  o: :i{intersection_iter:d} ;
  i: l: ;
- m: _:mb16cc_p .
-:to{intersection_iter:d} s: mb:{from_id_column:s} ;
+ m: _:linkset_prov_p.
+:to{intersection_iter:d} s: from:{from_id_column:s} ;
  p: tso: ;
- o: cc:{to_id_column:s};
+ o: to:{to_id_column:s};
  i: l: ;
- m: _:mb16cc_p .
+ m: _:mb16to_p .
 """
-mb_sf_within_template = """\
-:mw{within_iter:d} s: mb:{from_id_column:s} ;
+from_sf_within_template = """\
+:mw{within_iter:d} s: from:{from_id_column:s} ;
  p: w: ;
- o: cc:{to_id_column:s} ;
+ o: to:{to_id_column:s} ;
  i: l: ;
- m: _:mb16cc_p .
+ m: _:mb16to_p .
 """
-mb_sf_contains_template = """\
-:mc{within_iter:d} s: mb:{from_id_column:s} ;
+from_sf_contains_template = """\
+:mc{within_iter:d} s: from:{from_id_column:s} ;
  p: c: ;
- o: cc:{to_id_column:s} ;
+ o: to:{to_id_column:s} ;
  i: l: ;
- m: _:mb16cc_p .
+ m: _:mb16to_p .
 """
+
 def do_overlaps(from_id_column, to_id_column):
     logging.info("Generating overlaps statements")
     con = pg.connect("host={} port=5432 dbname=mydb user=postgres password=password".format(database_url))
@@ -130,8 +124,8 @@ def do_overlaps(from_id_column, to_id_column):
     """.format(from_id_column=from_id_column, to_id_column=to_id_column)
     c = 0
     intersection_iter = 0
-    expressed_mb_areas = set()
-    expressed_cc_areas = set()
+    expressed_from_areas = set()
+    expressed_to_areas = set()
     file_like = StringIO()
     cur.copy_expert("COPY ({}) TO STDOUT WITH CSV".format(command), file_like, size=32000)
     file_like.seek(0)
@@ -142,20 +136,20 @@ def do_overlaps(from_id_column, to_id_column):
             c += 1
             from_id_column = str(record[0])
             to_id_column = str(record[1])
-            mb_area_m2 = float(record[2])
-            cc_area_m2 = float(record[3])
+            from_area_m2 = float(record[2])
+            to_area_m2 = float(record[3])
             i_area_m2  = float(record[4])
-            mb_area_m2 = round((mb_area_m2 / 100.0), 7) * 100.0
-            cc_area_m2 = round((cc_area_m2 / 100.0), 7) * 100.0
+            from_area_m2 = round((from_area_m2 / 100.0), 7) * 100.0
+            to_area_m2 = round((to_area_m2 / 100.0), 7) * 100.0
             i_area_m2  = round((i_area_m2  / 100.0), 7) * 100.0
-            if from_id_column not in expressed_mb_areas:
-                mb_area_chunk = mb_area_template.format(from_id_column=from_id_column, mb_area_m2=mb_area_m2)
-                outfile.write(mb_area_chunk)
-                expressed_mb_areas.add(from_id_column)
-            if to_id_column not in expressed_cc_areas:
-                cc_area_chunk = cc_area_template.format(to_id_column=to_id_column, cc_area_m2=cc_area_m2)
-                outfile.write(cc_area_chunk)
-                expressed_cc_areas.add(to_id_column)
+            if from_id_column not in expressed_from_areas:
+                from_area_chunk = from_area_template.format(from_id_column=from_id_column, from_area_m2=from_area_m2)
+                outfile.write(from_area_chunk)
+                expressed_from_areas.add(from_id_column)
+            if to_id_column not in expressed_to_areas:
+                to_area_chunk = to_area_template.format(to_id_column=to_id_column, to_area_m2=to_area_m2)
+                outfile.write(to_area_chunk)
+                expressed_to_areas.add(to_id_column)
             next_chunk = overlaps_template.format(from_id_column=from_id_column, to_id_column=to_id_column, intersection_iter=intersection_iter, i_area_m2=i_area_m2)
             outfile.write(next_chunk)
             
@@ -182,30 +176,61 @@ def do_withins(from_id_column, to_id_column):
             within_iter += 1
             from_id_column = str(record[0])
             to_id_column = str(record[1])
-            mb_is_within = (str(record[2]).lower().startswith("t")) 
-            cc_is_within = (str(record[3]).lower().startswith("t")) 
-            if mb_is_within:
-                next_chunk = mb_sf_within_template.format(from_id_column=from_id_column, to_id_column=to_id_column, within_iter=within_iter)
+            from_is_within = (str(record[2]).lower().startswith("t")) 
+            to_is_within = (str(record[3]).lower().startswith("t")) 
+            if from_is_within:
+                next_chunk = from_sf_within_template.format(from_id_column=from_id_column, to_id_column=to_id_column, within_iter=within_iter)
             else:
-                next_chunk = mb_sf_contains_template.format(from_id_column=from_id_column, to_id_column=to_id_column, within_iter=within_iter)
+                next_chunk = from_sf_contains_template.format(from_id_column=from_id_column, to_id_column=to_id_column, within_iter=within_iter)
             outfile.write(next_chunk)
 
-def generate_header():
-    logging.info("Generating ttl header")
+
+def generate_header(header_description, header_title, contact_name, contact_email, from_prefix, to_prefix, subjects_target, objects_target, provenance_comment):
+    """
+    Genrates the turtle header describing this linkset
+    :param header_description: description of the linkset  
+    :param header_title: linkset title 
+    :param contact_name: contact name 
+    :param contact_email: contact email
+    :param from_prefix: prefix uris that identifiers will be appended to to create unique uris for from objects 
+    :param to_prefix: prefix uris that identifiers will be appended to to create unique uris for to objects 
+    :param subjects_target: uri that identifies the dataset that from objects reside in  
+    :param objects_target: uri that identifies the dataset that to objects reside in  
+    :param provenance_comment: description of the provenance information 
+    """
     with open("header.ttl", "w") as outfile:
-        header = header_template.format(contact_name=contact_name, contact_email=contact_email, creation_date=datetime.today().strftime('%Y-%m-%d'))
+        header = header_template.format(header_description=header_description, header_title=header_title, contact_name=contact_name, contact_email=contact_email,\
+        creation_date=datetime.today().strftime('%Y-%m-%d'), from_prefix=from_prefix, to_prefix=to_prefix, subjects_target=subjects_target, objects_target=objects_target, provenance_comment=provenance_comment)
         outfile.write(header)
 
-def concat_files():
+
+def concat_files(output_linkset_file_name):
+    """concatenates header within and overlaps statements into output ttl file"
+    :param output_linkset_file_name: output file name
+    """
     logging.info("Concatenating headers withins overlaps into compiled linkset file")
-    with open('ls_mb16to_t.ttl', 'w') as fout, fileinput.input(['./header.ttl', './within_all.ttl', './overlaps_all.ttl']) as fin:
+    with open(output_linkset_file_name, 'w') as fout, fileinput.input(['./header.ttl', './within_all.ttl', './overlaps_all.ttl']) as fin:
         for line in fin:
             fout.write(line)
 
 if __name__ == "__main__":
+    database_url = utils.fail_or_getenv('DATABASE_URL')
+    contact_name = utils.fail_or_getenv('CONTACT_NAME')
+    contact_email = utils.fail_or_getenv('CONTACT_EMAIL')
+    header_description = """This LOC-I Project Linkset relates Meshblock individuals in the ASGS LOC-I Dataset to Contracted Catchment individuals in the Geofabric LOC-I Dataset. All Meshblock -> Catchment relations are either transitiveSfOverlap, sfWithin, or sfContains. That is a Meshblock either overlaps or is wholly within the Catchment, or in some cases the Meshblock wholly contains the Catchment.
+    The Linkset triples (Meshblock sfWithin Catchment, Meshblock sfContains Catchment, Meshblock transitiveSfOverlap Catchment) are reified so that each triple is contained within an RDF Statement class instance so that the triple is numbered and the method used to generate the triple is given by the loci:hadGenerationMethod.
+    The method used for all triples in this Linkset is the same and it is SpatialIntersection which is defined below.
+    The triples for the main data in this linkset - the Statements relating Meshblocks to Catchments - are valid RDF in the Turtle syntax but an unusual namespacing arrangement is used so all terms are indicated with as few letters as possible, mostly one letter then colon, e.g. s: for http://www.w3.org/1999/02/22-rdf-syntax-ns#subject, rather than the more common rdf:subject. This is simply to reduce file size.
+    """
+    header_title = "Meshblocks Contracted Catchments Linkset"
     to_id_column = "hydroid"
     from_id_column = "mb_code_20"
+    subjects_target = "http://linked.data.gov.au/dataset/asgs2016"
+    objects_target = "http://linked.data.gov.au/dataset/geofabric"
+    from_prefix = "http://linked.data.gov.au/dataset/asgs2016/meshblock/"
+    to_prefix = "http://linked.data.gov.au/dataset/geofabric/contractedcatchment/"
+    provenance_comment = "The method used to classify Meshblock and Contracted Catchment relationships"
     do_withins(from_id_column, to_id_column)
     do_overlaps(from_id_column, to_id_column)
-    generate_header()
-    concat_files()
+    generate_header(header_description, header_title, contact_name, contact_email, from_prefix, to_prefix, subjects_target, objects_target, provenance_comment)
+    concat_files("ls_mb16cc.ttl")
