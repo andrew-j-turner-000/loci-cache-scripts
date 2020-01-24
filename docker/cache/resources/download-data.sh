89a15cf6a51590d7ad988cabcf50e795fa493a87
root <root@ip-172-31-16-77.ap-southeast-2.compute.internal>
2020-01-24 05:53:30 +0000

    Changes to download-data uris. All pointing to s3.

--- docker/cache/resources/download-data.sh
+++ docker/cache/resources/download-data.sh
@@ -3,11 +3,22 @@ set -e
 
 S3_BASE="https://${S3_BUCKET}.s3-ap-southeast-2.amazonaws.com"
 LINKSET_BASE="${S3_BASE}${S3_LINKSET_PATH}"
+MANUAL_LINKSET_BASE="${S3_BASE}${S3_MANUAL_LINKSET_PATH}"
 DATASET_BASE="${S3_BASE}${S3_DATASET_PATH}"
 
 #Linksets
-wget "${LINKSET_BASE}/${MB2CC_LINKSET_FILE}"
-wget "${LINKSET_BASE}/${ADDR2CC_LINKSET_FILE}"
+wget "https://ga-loci-neptune-test.s3-ap-southeast-2.amazonaws.com/ls_mb16cc.ttl"
+wget "https://ga-loci-neptune-test.s3-ap-southeast-2.amazonaws.com/trips_plus_ls_mb16cc.ttl"
+
+wget "https://ga-loci-neptune-test.s3-ap-southeast-2.amazonaws.com/ls_addrcatch.trig"
+
+wget "https://ga-loci-neptune-test.s3-ap-southeast-2.amazonaws.com/ls_addr201605catch.ttl"
+wget "https://ga-loci-neptune-test.s3-ap-southeast-2.amazonaws.com/trips_plus_ls_addr201605catch.ttl"
+
+wget "https://ga-loci-neptune-test.s3-ap-southeast-2.amazonaws.com/ls_addr1605mb16.ttl"
+wget "https://ga-loci-neptune-test.s3-ap-southeast-2.amazonaws.com/trips_plus_ls_addr1605mb16.ttl"
+
+# wget "${MANUAL_LINKSET_BASE}/${ADDR16052MB11_LINKSET_FILE}"
 
 #mb11mb16
 #addrcatch-linkset
@@ -16,46 +27,62 @@ wget "${LINKSET_BASE}/${ADDR2CC_LINKSET_FILE}"
 #addr1605mb11-linkset
 #addr1605mb16-linkset
 
-#Datasets
-wget "${DATASET_BASE}/${ASGS2016_DATASET_FILE}"
-wget "${DATASET_BASE}/${GEOFABRIC_DATASET_FILE}"
-wget "${DATASET_BASE}/${GNAF_DATASET_FILE}"
+#Datasets and Register Info
+wget "https://ga-loci-neptune-test.s3-ap-southeast-2.amazonaws.com/asgs2016.ttl"
+wget "https://ga-loci-neptune-test.s3-ap-southeast-2.amazonaws.com/trips_plus_asgs2016.trig"
 
-#Registries
-wget "http://linked.data.gov.au/dataset/asgs2016/reg/?_view=reg&_format=text/turtle" -O asgs2016.reg.ttl
-wget "http://linked.data.gov.au/dataset/geofabric/?_view=reg&_format=text/turtle" -O geofabric.reg.ttl
-wget "http://linked.data.gov.au/dataset/gnaf/?_view=reg&_format=text/turtle" -O gnafCurrent.reg.ttl
+wget "https://ga-loci-neptune-test.s3-ap-southeast-2.amazonaws.com/asgs2016.reg.ttl" -O asgs2016.reg.ttl
+
+wget "https://ga-loci-neptune-test.s3-ap-southeast-2.amazonaws.com/geofabric2.ttl"
+wget "https://ga-loci-neptune-test.s3-ap-southeast-2.amazonaws.com/geofabric.reg.ttl" -O geofabric.reg.ttl
+wget "https://ga-loci-neptune-test.s3-ap-southeast-2.amazonaws.com/trips_plus_geofabric2.trig"
+
+#wget "https://ga-loci-neptune-test.s3-ap-southeast-2.amazonaws.com/gnaf_current.ttl"
+# wget "http://linked.data.gov.au/dataset/gnaf/?_view=reg&_format=text/turtle" -O gnafCurrent.reg.ttl
+
+wget "https://ga-loci-neptune-test.s3-ap-southeast-2.amazonaws.com/gnaf_201605_addressSites_instances.nt.gz"
+wget "https://ga-loci-neptune-test.s3-ap-southeast-2.amazonaws.com/trips_plus_gnaf_201605_addressSites_instances.nt"
+
+wget "https://ga-loci-neptune-test.s3-ap-southeast-2.amazonaws.com/gnaf_201605_address_instances.nt.gz"
+
+wget "https://ga-loci-neptune-test.s3-ap-southeast-2.amazonaws.com/gnaf_201605_locality_instances.nt.gz"
+wget "https://ga-loci-neptune-test.s3-ap-southeast-2.amazonaws.com/trips_plus_gnaf_201605_locality_instances.nt"
+
+wget "https://ga-loci-neptune-test.s3-ap-southeast-2.amazonaws.com/gnaf_201605_streetLocality_instances.nt.gz"
+wget "https://ga-loci-neptune-test.s3-ap-southeast-2.amazonaws.com/trips_plus_gnaf_201605_streetLocality_instances.nt"
+
+wget "https://ga-loci-neptune-test.s3-ap-southeast-2.amazonaws.com/gnaf201605.reg.ttl" -O gnaf201605.reg.ttl
+
 
 #Externally published Ontologies
-wget --header="Accept: text/turtle" http://linked.data.gov.au/def/asgs -O asgs.ont.ttl
-# wget --header="Accept: text/turtle" https://raw.githubusercontent.com/AGLDWG/asgs-ont/master/asgs.ttl -O asgs.ont.ttl
-# wget "https://loci-assets.s3-ap-southeast-2.amazonaws.com/ontologies/loci-lite/asgs.ttl" -O asgs.ont.ttl
-wget --header="Accept: text/turtle" http://linked.data.gov.au/def/gnaf -O gnaf.ont.ttl
-# wget --header="Accept: text/turtle" https://raw.githubusercontent.com/AGLDWG/gnaf-ont/master/gnaf.ttl -O gnaf.ont.ttl
-# wget "https://loci-assets.s3-ap-southeast-2.amazonaws.com/ontologies/loci-lite/gnaf.ttl" -O gnaf.ont.ttl
-# wget --header="Accept: text/turtle" http://linked.data.gov.au/def/geofabric -O geofabric.ont.ttl
-wget --header="Accept: text/turtle" http://linked.data.gov.au/def/geofabric -O geofabric.ont.ttl
+wget --header="Accept: text/turtle" https://ga-loci-neptune-test.s3-ap-southeast-2.amazonaws.com/asgs.ont.ttl -O asgs.ont.ttl
 
-# wget --header="Accept: text/turtle" https://raw.githubusercontent.com/CSIRO-enviro-informatics/geofabric-ont/master/geofabric.ttl -O gnaf.ont.ttl
-# wget "https://loci-assets.s3-ap-southeast-2.amazonaws.com/ontologies/loci-lite/geofabric.ttl" -O geofabric.ont.ttl
-wget "https://www.opengis.net/def/appschema/hy_features/hyf/hyf.ttl" -O hy_features.ont.ttl
-wget --header="Accept: text/turtle" http://qudt.org/2.0/schema/SCHEMA_QUDT-v2.0.ttl -O SCHEMA_QUDT-v2.1.ont.ttl
-# wget "https://loci-assets.s3-ap-southeast-2.amazonaws.com/ontologies/loci-lite/SCHEMA_QUDT-v2.1.ttl" -O SCHEMA_QUDT-v2.1.ont.ttl
-wget --header="Accept: text/turtle" http://www.w3.org/ns/org -O org.ont.ttl
-# wget "https://loci-assets.s3-ap-southeast-2.amazonaws.com/ontologies/loci-lite/org.ttl" -O org.ont.ttl
+wget --header="Accept: text/turtle" https://ga-loci-neptune-test.s3-ap-southeast-2.amazonaws.com/gnaf.ont.ttl -O gnaf.ont.ttl
+
+wget --header="Accept: text/turtle" https://ga-loci-neptune-test.s3-ap-southeast-2.amazonaws.com/geofabric.ont.ttl -O geofabric.ont.ttl
+
+wget "https://ga-loci-neptune-test.s3-ap-southeast-2.amazonaws.com/hy_features.ont.ttl" -O hy_features.ont.ttl
+
+wget --header="Accept: text/turtle" https://ga-loci-neptune-test.s3-ap-southeast-2.amazonaws.com/SCHEMA_QUDT-v2.1.ont.ttl -O SCHEMA_QUDT-v2.1.ont.ttl
+
+wget --header="Accept: text/turtle" https://ga-loci-neptune-test.s3-ap-southeast-2.amazonaws.com/org.ont.ttl -O org.ont.ttl
+
 wget --header="Accept: text/turtle" http://rdfs.org/ns/void -O void.ont.ttl
-# wget "https://loci-assets.s3-ap-southeast-2.amazonaws.com/ontologies/loci-lite/void.ttl" -O void.ont.ttl
-wget --header="Accept: text/turtle" http://www.w3.org/ns/prov -O prov-o.ont.ttl
-# wget "https://loci-assets.s3-ap-southeast-2.amazonaws.com/ontologies/loci-lite/prov-o.ttl" -O prov-o.ont.ttl
-wget --header="Accept: application/rdf+xml" http://www.opengis.net/ont/geosparql -O geo.ont.rdf
-# wget "https://loci-assets.s3-ap-southeast-2.amazonaws.com/ontologies/loci-lite/geosparql.rdf" -O geo.ont.rdf
-wget --header="Accept: text/turtle" http://purl.org/linked-data/registry -O registry.ont.ttl
+
+wget --header="Accept: text/turtle" https://ga-loci-neptune-test.s3-ap-southeast-2.amazonaws.com/prov-o.ont.ttl -O prov-o.ont.ttl
+
+wget --header="Accept: application/rdf+xml" https://ga-loci-neptune-test.s3-ap-southeast-2.amazonaws.com/geo.ont.rdf -O geo.ont.rdf
+
+wget --header="Accept: text/turtle" https://ga-loci-neptune-test.s3-ap-southeast-2.amazonaws.com/registry.ont.ttl -O registry.ont.ttl
 
 #Local ontologies
-# wget http://www.linked.data.gov.au/def/loci/loci.ttl -O loci.ont.ttl
-wget --header="Accept: text/turtle" https://raw.githubusercontent.com/CSIRO-enviro-informatics/loci-ont/master/loci.ttl -O loci.ont.ttl
-# wget "https://loci-assets.s3-ap-southeast-2.amazonaws.com/ontologies/loci-lite/loci.ttl" -O loci.ont.ttl
-wget --header="Accept: text/turtle" https://raw.githubusercontent.com/CSIRO-enviro-informatics/geosparql-ext-ont/master/geox.ttl -O geox.ont.ttl
-# wget "https://loci-assets.s3-ap-southeast-2.amazonaws.com/ontologies/loci-lite/geosparql-ext.ttl" -O geox.ont.ttl
+
+wget --header="Accept: text/turtle" https://ga-loci-neptune-test.s3-ap-southeast-2.amazonaws.com/loci.ont.ttl -O loci.ont.ttl
+
+wget --header="Accept: text/turtle" https://ga-loci-neptune-test.s3-ap-southeast-2.amazonaws.com/geox.ont.ttl -O geox.ont.ttl
 
 
+if ls *.html 1> /dev/null 2>&1; then
+    echo "Likely and error in the downloads, check the URLs"
+    exit 1
+fi
